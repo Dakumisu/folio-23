8045d1c1ae7abe1c31b0c937d85609050589095c
Alex <alex.gattefosse@gmail.com>
2023-05-31 10:07:47 +0200

    clean material texture

--- apps/client/webgl/components/PixelNoisePlane.js
+++ apps/client/webgl/components/PixelNoisePlane.js
@@ -1,36 +1,19 @@
-import { Mesh, Plane, Texture } from '@lm/le-webgl';
+import { Mesh, Plane } from '@lm/le-webgl';
 import BaseComponent from '~webgl/core/BaseComponent';
 import { PixelNoiseMaterial } from '~webgl/shaders/materials/PixelNoiseMaterial';
 
-import noiseTexture from '~assets/canvas/perlin-noise.png?url';
 
 export default class PixelNoisePlane extends BaseComponent {
 	init() {
 		const { ctx } = this.webgl; //TODO: make gl global in OGL
 
 		const geometry = new Plane(ctx);
-
-		const texture = new Texture(ctx, {
-			wrapS: ctx.REPEAT,
-			wrapT: ctx.REPEAT,
-			generateMipmaps: false,
-		});
-
-		const program = new PixelNoiseMaterial(ctx, {
-			uniforms: {
-				noiseTexture: { value: texture },
-			},
-		});
+		const program = new PixelNoiseMaterial(ctx);
 
 		const mesh = new Mesh(ctx, { geometry, program });
 		// mesh.setParent(this.parent.instance);
 
 		this.base = mesh;
-
-		// update image value with source once loaded
-		const img = new Image();
-		img.src = noiseTexture;
-		img.onload = () => texture.image = img;
 	}
 
 	update() {
--- apps/client/webgl/shaders/materials/PixelNoiseMaterial.js
+++ apps/client/webgl/shaders/materials/PixelNoiseMaterial.js
@@ -1,17 +1,31 @@
-import { Program } from '@lm/le-webgl';
+import { Program, Texture } from '@lm/le-webgl';
 import { webgl } from '~webgl/core';
 
 import fs from './PixelNoise.frag';
 import vs from './PixelNoise.vert';
 
+import noiseTexture from '~assets/canvas/perlin-noise.png?url';
+
 export class PixelNoiseMaterial extends Program {
 	constructor(gl, args = {}) {
+		const texture = new Texture(gl, {
+			wrapS: gl.REPEAT,
+			wrapT: gl.REPEAT,
+			generateMipmaps: false,
+		});
+
+		// update image value with source once loaded
+		const img = new Image();
+		img.src = noiseTexture;
+		img.onload = () => texture.image = img;
+
 		super(gl, {
 			vertex: vs,
 			fragment: fs,
 			uniforms: {
 				...webgl.uniforms,
-				...args.uniforms
+				...args.uniforms,
+				noiseTexture: { value: texture },
 			},
 		});
 	}
